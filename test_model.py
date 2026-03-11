815749abc921cb8aa03aee68c5280915697bc9f4
lealcristopher <cristopher.leal@proton.me>
2026-03-11 16:32:25 -0300

    test push

--- test_model.py
+++ test_model.py
@@ -1,6 +1,7 @@
 from model import get_user_data
 
 def test_user_structure():
+    a = 1
     data = get_user_data()
     assert data == {"id": 1, "name": "Cristopher"}
     assert isinstance(data, dict)
